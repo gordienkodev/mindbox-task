0d0dea573e7901c9177695c31fefc45e41da1cc9
Pavel Gordienko <guzmo@yandex.ru>
2024-10-19 00:55:34 +0300

    feat: add block tasks and styled

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "@fontsource/roboto/100.css";
 import TodoInput from "./components/TodoInput";
-import { Stack } from "@mui/material";
+import { Stack, Box } from "@mui/material";
 
 const theme = createTheme({
   typography: {
@@ -15,25 +15,37 @@ const theme = createTheme({
     },
   },
   components: {
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          "& fieldset": {
+            borderColor: "#E5E5E5",
+          },
+          "&:hover fieldset": {
+            borderColor: "#3A3A3A",
+          },
+        },
+      },
+    },
     MuiInputBase: {
       styleOverrides: {
         input: {
-          fontSize: "3rem",
+          fontSize: "2.5rem",
           "&::placeholder": {
             fontStyle: "italic",
             color: "#E5E5E5",
           },
-          background: '#FEFEFE'
+          background: "#FEFEFE",
         },
       },
     },
     MuiInputLabel: {
       styleOverrides: {
         root: {
-          fontSize: "3rem",
+          fontSize: "2.5rem",
           fontStyle: "italic",
           color: "#E5E5E5",
-          background: '#FEFEFE'
+          background: "#FEFEFE",
         },
       },
     },
@@ -52,7 +64,38 @@ function App() {
         }}
       >
         <Typography variant="h1">todos</Typography>
-        <TodoInput />
+        <Box
+          sx={{
+            boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
+            background: "#FEFEFE",
+          }}
+        >
+          <TodoInput />
+        </Box>
+        <Box
+          sx={{
+            boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
+            background: "#FEFEFE",
+            position: "relative",
+            zIndex: -1,
+            width: "99%",
+            margin: "0 auto",
+          }}
+        >
+          <Typography sx={{ padding: "3px" }}></Typography>
+        </Box>
+        <Box
+          sx={{
+            boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
+            background: "#FEFEFE",
+            position: "relative",
+            zIndex: -2,
+            width: "98%",
+            margin: "0 auto",
+          }}
+        >
+          <Typography sx={{ padding: "3px" }}></Typography>
+        </Box>
       </Stack>
     </ThemeProvider>
   );
--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -3,16 +3,43 @@ import TextField from "@mui/material/TextField";
 import CheckIcon from "@mui/icons-material/Check";
 import InputAdornment from "@mui/material/InputAdornment";
 import { styled } from "@mui/material/styles";
+import { Box } from "@mui/material";
+import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
 
 const StyledCheckIcon = styled(CheckIcon)({
   color: "#E6E6E6",
-  background: '#FEFEFE',
-  paddingRight: '20px',
-  fontSize: "2.5rem",
+  background: "#FEFEFE",
+  paddingRight: "10px",
+  paddingLeft: "10px",
+  fontSize: "3rem",
 });
+
+const StyledRadioButtonUncheckedIcon = styled(RadioButtonUncheckedIcon)({
+  color: "#E6E6E6",
+  background: "#FEFEFE",
+  paddingRight: "10px",
+  paddingLeft: "10px",
+  fontSize: "3rem",
+});
+
+const TaskBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: "10px",
+  border: "1px solid #E5E5E5",
+  background: "#FEFEFE",
+  color: "#4D4D4D",
+  fontSize: "2.5rem",
+  fontFamily: "Roboto, sans-serif",
+}));
 
 const TodoInput: React.FC = () => {
   const [task, setTask] = useState<string>("");
+  const [tasks, setTasks] = useState<string[]>([
+    "Тестовое задание",
+    "Прекрасный код",
+    "Покрытие тестами",
+  ]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setTask(event.target.value);
@@ -20,28 +47,46 @@ const TodoInput: React.FC = () => {
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setTask("");
+    if (task.trim() !== "") {
+      setTasks((prevTasks) => [...prevTasks, task]);
+      setTask("");
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <TextField
-        id="outlined-basic"
-        placeholder="What needs to be done?"
-        variant="outlined"
-        value={task}
-        onChange={handleChange}
-        fullWidth
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <StyledCheckIcon />
-            </InputAdornment>
-          ),
-          style: { paddingLeft: "20px", background: '#FEFEFE',  color: "#4D4D4D",},
-        }}
-      />
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="outlined-basic"
+          placeholder="What needs to be done?"
+          variant="outlined"
+          value={task}
+          onChange={handleChange}
+          fullWidth
+          slotProps={{
+            input: {
+              startAdornment: (
+                <InputAdornment position="start">
+                  <StyledCheckIcon />
+                </InputAdornment>
+              ),
+              style: {
+                background: "#FEFEFE",
+                color: "#4D4D4D",
+              },
+            },
+          }}
+        />
+      </form>
+      {tasks.map((t, index) => (
+        <TaskBox key={index}>
+          <InputAdornment position="start">
+            <StyledRadioButtonUncheckedIcon />
+          </InputAdornment>
+          {t}
+        </TaskBox>
+      ))}
+    </div>
   );
 };
 

f157f9b585835bf37fc99895d47ee462f126f4e6
Pavel Gordienko <guzmo@yandex.ru>
2024-10-19 19:38:58 +0300

    feat: add icons

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -4,7 +4,8 @@ import CheckIcon from "@mui/icons-material/Check";
 import InputAdornment from "@mui/material/InputAdornment";
 import { styled } from "@mui/material/styles";
 import { Box } from "@mui/material";
-import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
+import CheckMarkIcon from "./CheckMarkIcon";
+import GrayCircleIcon from "./GrayCircleIcon";
 
 const StyledCheckIcon = styled(CheckIcon)({
   color: "#E6E6E6",
@@ -14,31 +15,26 @@ const StyledCheckIcon = styled(CheckIcon)({
   fontSize: "3rem",
 });
 
-const StyledRadioButtonUncheckedIcon = styled(RadioButtonUncheckedIcon)({
-  color: "#E6E6E6",
-  background: "#FEFEFE",
-  paddingRight: "10px",
-  paddingLeft: "10px",
-  fontSize: "3rem",
-});
-
-const TaskBox = styled(Box)(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  padding: "10px",
-  border: "1px solid #E5E5E5",
-  background: "#FEFEFE",
-  color: "#4D4D4D",
-  fontSize: "2.5rem",
-  fontFamily: "Roboto, sans-serif",
-}));
+const TaskBox = styled(Box)<{ completed: boolean }>(({ completed }) => ({
+    display: "flex",
+    alignItems: "center",
+    padding: "10px",
+    border: "1px solid #E5E5E5",
+    background: "#FEFEFE",
+    color: completed ? "#E6E6E6" : "#4D4D4D", 
+    fontSize: "2.5rem",
+    fontFamily: "Roboto, sans-serif",
+    cursor: "pointer",
+    textDecoration: completed ? "line-through" : "none",
+    textDecorationThickness: completed ? "2px" : "0",
+  }));
 
 const TodoInput: React.FC = () => {
   const [task, setTask] = useState<string>("");
-  const [tasks, setTasks] = useState<string[]>([
-    "Тестовое задание",
-    "Прекрасный код",
-    "Покрытие тестами",
+  const [tasks, setTasks] = useState<{ text: string; completed: boolean }[]>([
+    { text: "Тестовое задание", completed: false },
+    { text: "Прекрасный код", completed: false },
+    { text: "Покрытие тестами", completed: false },
   ]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -48,13 +44,21 @@ const TodoInput: React.FC = () => {
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (task.trim() !== "") {
-      setTasks((prevTasks) => [...prevTasks, task]);
+      setTasks((prevTasks) => [...prevTasks, { text: task, completed: false }]);
       setTask("");
     }
   };
 
+  const toggleTaskCompletion = (index: number) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task, i) =>
+        i === index ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
   return (
-    <div>
+    <>
       <form onSubmit={handleSubmit}>
         <TextField
           id="outlined-basic"
@@ -79,14 +83,14 @@ const TodoInput: React.FC = () => {
         />
       </form>
       {tasks.map((t, index) => (
-        <TaskBox key={index}>
+        <TaskBox key={index} onClick={() => toggleTaskCompletion(index)} completed={t.completed}>
           <InputAdornment position="start">
-            <StyledRadioButtonUncheckedIcon />
+            {t.completed ? <CheckMarkIcon /> : <GrayCircleIcon />}
           </InputAdornment>
-          {t}
+          {t.text}
         </TaskBox>
       ))}
-    </div>
+    </>
   );
 };
 
